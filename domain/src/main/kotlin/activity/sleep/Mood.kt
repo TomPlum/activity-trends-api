6eefacc7cf2819d33ec02f30cc12d87821c3bd08
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-09-13 15:01:37 +0100

    Domain | Added Mood fromString & Tested

--- domain/src/main/kotlin/activity/sleep/Mood.kt
+++ domain/src/main/kotlin/activity/sleep/Mood.kt
@@ -6,5 +6,16 @@ enum class Mood(val value: String) {
     OK("Ok"),
     NOT_SO_GOOD("Not Good"),
     BAD("Bad"),
-    UNKNOWN("N/A")
+    UNKNOWN("N/A");
+
+    companion object {
+        fun fromString(value: String?) = when(value) {
+            "Great" -> GREAT
+            "Good" -> GOOD
+            "Ok" -> OK
+            "Not Good" -> NOT_SO_GOOD
+            "Bad" -> BAD
+            else -> UNKNOWN
+        }
+    }
 }
