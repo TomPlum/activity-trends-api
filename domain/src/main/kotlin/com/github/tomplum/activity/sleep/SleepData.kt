9a3367d49d23fa2c6dfeec26b2d8ddd2ba3d3b3c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-09-30 23:43:25 +0100

    Domain | Started Implementing SleepData Business Logic

--- domain/src/main/kotlin/com/github/tomplum/activity/sleep/SleepData.kt
+++ domain/src/main/kotlin/com/github/tomplum/activity/sleep/SleepData.kt
@@ -1,3 +1,7 @@
 package com.github.tomplum.activity.sleep
 
-data class SleepData(val snapshots: List<SleepSnapshot>)
+data class SleepData(val snapshots: List<SleepSnapshot>) {
+    fun getDates() = snapshots.map { it.date }
+
+    fun getMostRecentSnapshot() = snapshots.maxByOrNull { it.date }
+}
---
+++ domain/src/test/kotlin/com/github/tomplum/activity/sleep/SleepDataTest.kt
@@ -0,0 +1,14 @@
+package com.github.tomplum.activity.sleep
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class SleepDataTest {
+    @Nested
+    inner class GetSnapshotDates {
+        @Test
+        fun singleSnapshot() {
+
+        }
+    }
+}
