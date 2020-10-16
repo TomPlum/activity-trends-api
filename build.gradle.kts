7d4218501ebb0499bd7d432618dba3f7a10d64fd
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-10-16 18:33:24 +0100

    Spring | Added Actuator Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
