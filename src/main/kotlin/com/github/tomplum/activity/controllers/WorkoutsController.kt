b2454c24c2787dfa7beabbe12b1abf9dd97fc2ba
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-01-23 11:58:13 +0000

    Presentation | Added Workouts Controller Endpoint Mappings

--- src/main/kotlin/com/github/tomplum/activity/controllers/WorkoutsController.kt
+++ src/main/kotlin/com/github/tomplum/activity/controllers/WorkoutsController.kt
@@ -4,12 +4,16 @@ import com.github.tomplum.activity.converters.service.WorkoutConversionService
 import com.github.tomplum.activity.dto.workouts.WorkoutSessionResponse
 import com.github.tomplum.activity.services.WorkoutService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 @RestController
+@RequestMapping("/workouts")
 class WorkoutsController(private val service: WorkoutService, private val converter: WorkoutConversionService) {
+    @GetMapping("/sessions")
     fun getWorkoutSessions(): ResponseEntity<WorkoutSessionResponse> {
         val sessions = service.getWorkoutSessions()
         val response = converter.convert(sessions, WorkoutSessionResponse::class.java)!!
